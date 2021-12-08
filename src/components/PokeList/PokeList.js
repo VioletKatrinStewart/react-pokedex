4464971511847cf44a7a4af36e290d2f39a54518
Violet Stewart <vkazoo2@gmail.com>
2021-12-08 14:54:50 -0800

    caught up to point that was demod search querys working

---
+++ src/components/PokeList/PokeList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function PokeList({ pokemon }) {
+  return (
+    <div>
+      {' '}
+      {pokemon.map((poke) => (
+        <p key={poke.id}>{poke.pokemon}</p>
+      ))}
+    </div>
+  );
+}
